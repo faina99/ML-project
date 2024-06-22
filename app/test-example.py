c1ad87382eb12fda4be3b6610cd7fe7e6960187a
Benoit Pimpaud <pimpaudben@gmail.com>
2024-06-22 16:10:32 +0200

    [feature] clean up

--- app/test-example.py
+++
@@ -1,8 +0,0 @@
-
-def add(a, b):
-    return a + b
-
-def test_add():
-    a = 3
-    b = 2
-    assert add(a, b) == 5 
---
+++ test-exercice/operations.py
@@ -0,0 +1,15 @@
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise Exception("Can't divide by 0")
+        
+    return a / b

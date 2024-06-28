60b159849231d20cdd9fef67af7eb5098aff9d5e
Faina AIT HAMMOUDA <faina_ath@MacBook-Pro-de-Faina.local>
2024-06-28 11:23:57 +0200

    Initial commit

--- test-exercice/main.py
+++
@@ -1,9 +0,0 @@
-from operations import add, sub, multiply, divide
-
-if __name__ == "__main__":
-    a, b = 4, 2
-    print("Hello")
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} x {b} = {multiply(a, b)}")
-    print(f"{a} / {b} = {divide(a, b)}")
